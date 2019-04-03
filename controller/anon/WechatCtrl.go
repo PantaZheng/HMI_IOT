ecc6036e17bdbe344c685cd0a8038cdd6354d6ce
PantaZheng <peter19950105@gmail.com>
2019-04-03 13:11:32 +0800

    menu

--- controller/anon/WechatCtrl.go
+++ controller/anon/WechatCtrl.go
@@ -4,19 +4,21 @@ import (
 	"fmt"
 	"github.com/kataras/iris"
 	"github.com/silenceper/wechat"
+	"github.com/silenceper/wechat/menu"
 	"github.com/silenceper/wechat/message"
 )
 
-//101.132.125.102
+const weburl="101.132.125.102"
+
+var config = &wechat.Config{
+	AppID: "wx6bb6950cf39d79ee",
+	AppSecret: "25e017d8ab0f6711b5080be1ae317421",
+	Token: "HMIIoT",
+	EncodingAESKey:"iesxoHBsnaKVry5E8xd8gavmJLTVVNcd8aS7w3KYOaU",
+}
 
 func Login(ctx iris.Context) {
 	// wechat parameters
-	config := &wechat.Config{
-		AppID: "wx6bb6950cf39d79ee",
-		AppSecret: "25e017d8ab0f6711b5080be1ae317421",
-		Token: "HMIIoT",
-		EncodingAESKey:"iesxoHBsnaKVry5E8xd8gavmJLTVVNcd8aS7w3KYOaU",
-	}
 
 	wc := wechat.NewWechat(config)
 
@@ -30,15 +32,33 @@ func Login(ctx iris.Context) {
 	}
 	//发送回复的消息
 	_=wechatServer.Send()
+
+	wechatMenu := wc.GetMenu()
+
+	btnPlaceholder := new (menu.Button)
+	btnPlaceholder.SetViewButton("项目/任务","www.baidu.com")
+	btnCreate := new (menu.Button)
+	btnCreate.SetViewButton("创建用户","www.qq.com")
+	btnWeekly := new (menu.Button)
+	btnWeekly.SetViewButton("周报","www.github.com")
+	buttonsSub := make([]* menu.Button,2)
+	buttonsSub[0]=btnCreate
+	buttonsSub[1]=btnWeekly
+	btnPerson := new (menu.Button)
+	btnPerson.SetClickButton("个人","person")
+	btnPerson.SetSubButton("subButton",buttonsSub)
+	defaultButtons := make ([]* menu.Button, 2)
+	defaultButtons[0]=btnPlaceholder
+	defaultButtons[1]=btnPerson
+
+	err = wechatMenu.SetMenu(defaultButtons)
+	if err != nil {
+		fmt.Printf("err= %v", err)
+		return
+	}
 }
 
 func Port(ctx iris.Context){
-	config := &wechat.Config{
-		AppID: "wx6bb6950cf39d79ee",
-		AppSecret: "25e017d8ab0f6711b5080be1ae317421",
-		Token: "HMIIoT",
-		EncodingAESKey:"iesxoHBsnaKVry5E8xd8gavmJLTVVNcd8aS7w3KYOaU",
-	}
 
 	wc := wechat.NewWechat(config)
 	wechatServer := wc.GetServer(ctx.Request(),ctx.ResponseWriter())
