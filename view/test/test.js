7a8d853d13515265aa0880b02b46edc010ca4f62
PantaZheng <peter19950105@gmail.com>
2019-04-09 19:21:08 +0800

    url

--- view/test/test.js
+++ view/test/test.js
@@ -2,13 +2,7 @@ function Ctrl($scope, $http, $location, $window) {
     // $.showLoading();
     
 	$scope.getId=function(){
-		if ($window.sessionStorage.weChatId == null) {
-		    var weChatId = $location.search().weChatId;
-		    $window.sessionStorage.weChatId = weChatId;
-		}
-		// alert("jjj")
-		alert($window.sessionStorage.weChatId);	
+		var url=$location.absUrl();
+		alert(url);
 	}
-	
-	
 }
