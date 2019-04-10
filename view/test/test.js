afa1722a50ffdd3d133900ce574ebbc24dcddd68
PantaZheng <peter19950105@gmail.com>
2019-04-10 13:41:11 +0800

    createUser

--- view/test/test.js
+++ view/test/test.js
@@ -1,24 +1,16 @@
+var app=angular.module("testapp",[]);
+app.config(['$locationProvider',function($locationProvider){
+	$locationProvider.html5Mode({
+		enabled:true,
+		requireBase:false
+	});
+}])
+
 function Ctrl($scope, $http, $location, $window) {
-    // $.showLoading();
-//     var url=$location.url();
-// 	alert(url);	
-	// $scope.getId=function(){
-// 		if ($window.sessionStorage.weChatId == null) {
-// 		    var weChatId = $location.search().weChatId;
-// 		    $window.sessionStorage.weChatId = weChatId;
-// 		}
-		var url=$location.absUrl();
-		var url2=$location.search().code;
-		var c=$location.search()['code'];
-		
-		$scope.infor=JSON.stringify(url);
-		$scope.code=JSON.stringify(url2);
-		$scope.code1=JSON.stringify(c);		
-		// $scope.w=JSON.stringify("code=dsdsd&appid=fdfdfd");
-		alert(c);	
-		// alert("jjj")
-		// alert($window.sessionStorage.weChatId);	
-	// }
+	var url=$location.absUrl();
+	var url2=$location.search().code;
 	
-	
+	$scope.infor=JSON.stringify(url);
+	$scope.code=JSON.stringify(url2);
+	$scope.code2=JSON.stringify($location.search().code);		
 }
