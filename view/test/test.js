cea1c52feb35c5a8747d0f318903ac0a57b3b0dc
PantaZheng <peter19950105@gmail.com>
2019-04-10 17:08:21 +0800

    testMenu

--- view/test/test.js
+++ view/test/test.js
@@ -7,10 +7,32 @@ app.config(['$locationProvider',function($locationProvider){
 }])
 
 function Ctrl($scope, $http, $location, $window) {
-	var url=$location.absUrl();
-	var url2=$location.search().code;
-	
-	$scope.infor=JSON.stringify(url);
-	$scope.code=JSON.stringify(url2);
+	var code=$location.search().code;
 	$scope.code2=JSON.stringify($location.search().code);		
+    $http({
+        url: " https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx2203c68c9311ea43&secret=40c40547e174ed99d1281b2890f7eeb3&code="+$scope.code+"&grant_type=authorization_code",
+        method: 'get',
+        // headers: {token: $window.sessionStorage.weChatId}
+    }).success(function (res) {
+        // console.log(res);
+        $scope.infor = JSON.stringify(res);
+		alert($scope.infor);
+        // $window.sessionStorage.user = JSON.stringify(res);
+        // $scope.getProject(res.role, res.level);
+    }).error(function (err) {
+        $.hideLoading();
+        $.alert(err);
+    })
+    $http({
+        url: " https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx2203c68c9311ea43&secret=40c40547e174ed99d1281b2890f7eeb3&code="+code+"&grant_type=authorization_code",
+        method: 'get',
+        // headers: {token: $window.sessionStorage.weChatId}
+    }).success(function (res) {
+        // console.log(res);
+        $scope.code = JSON.stringify(res);
+		// alert($scope.infor);
+    }).error(function (err) {
+        $.hideLoading();
+        $.alert(err);
+    })		
 }
