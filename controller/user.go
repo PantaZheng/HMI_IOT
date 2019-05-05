8a3eb8a2095120b0bdccc8e5a748518df845f324
PantaZheng <peter19950105@gmail.com>
2019-05-05 15:42:53 +0800

    测试

--- controller/user.go
+++ controller/user.go
@@ -6,18 +6,18 @@ import (
 	"github.com/pantazheng/bci/service"
 )
 
-func Enroll(ctx iris.Context) {
-	userEnroll :=&models.User{}
-	if err:=ctx.ReadJSON(userEnroll);err!=nil{
-		panic(err.Error())
-	}
-	returnId:=&returnId{}
-	returnId.OpenId=service.Enroll(userEnroll)
-	ctx.StatusCode(iris.StatusOK)
-	if _,err:=ctx.JSON(returnId);err!=nil{
-		panic(err.Error())
-	}
-}
+//func Enroll(ctx iris.Context) {
+//	userEnroll :=&models.User{}
+//	if err:=ctx.ReadJSON(userEnroll);err!=nil{
+//		panic(err.Error())
+//	}
+//	returnId:=&returnId{}
+//	returnId.OpenId=service.Enroll(userEnroll)
+//	ctx.StatusCode(iris.StatusOK)
+//	if _,err:=ctx.JSON(returnId);err!=nil{
+//		panic(err.Error())
+//	}
+//}
 
 
 
