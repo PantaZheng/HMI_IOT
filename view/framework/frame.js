515a9fe3e678cde12e8e628ecaaf9b29a055ff0f
pantazheng <pantazheng@vip.qq.com>
2019-05-05 21:53:18 +0800

    删除view

--- view/framework/frame.js
+++
@@ -1,8 +0,0 @@
-app.controller('ctrl',function($scope, $http,$location, $window) {
-    $http.get("../testData/member.json"
-    ).then(function(results){
-    	$scope.list=results.data.data; 
-		console.log($scope.list)
-    }); 
-
-})
--- view/js/app.js
+++
@@ -1,7 +0,0 @@
-var app=angular.module("app",[]);
-app.config(['$locationProvider',function($locationProvider){
-	$locationProvider.html5Mode({
-		enabled:true,
-		requireBase:false
-	});
-}])
