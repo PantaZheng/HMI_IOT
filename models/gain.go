072abcdf1fa6ac9c48cb18509848898aae9af858
PantaZheng <peter19950105@gmail.com>
2019-05-15 02:03:38 +0800

    models 对象

--- models/gain.go
+++ models/gain.go
@@ -103,15 +103,12 @@ func GainsFindByOwnerID(id uint) (gains []Gain, err error) {
 	if err = owner.First(); err == nil {
 		if err = database.DB.Model(&owner).Related(&gains, "OwnerID").Error; err == nil {
 			err = owner.First()
-			log.Println("循环前")
-			log.Println(owner)
 			for _, v := range gains {
-				v.Owner = owner
+				&v.Owner = &owner
 				log.Println("循环中")
-				log.Println(owner)
+				log.Println(v.Owner)
 			}
 			log.Println("循环后")
-			log.Println(owner)
 			log.Println(gains)
 		}
 	}
