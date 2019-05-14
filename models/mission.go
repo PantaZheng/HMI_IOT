c85ba3f1f53d671d3689f348f7ea8090fda50c8c
PantaZheng <peter19950105@gmail.com>
2019-05-15 01:34:32 +0800

    models 指指针，service对象

--- models/mission.go
+++ models/mission.go
@@ -42,7 +42,10 @@ func (mission *Mission) Create() (err error) {
 	}
 	mission.CreateTime = time.Now().Format("2006-01-02")
 	participants := mission.Participants
-	mission.Participants = make([]*User, 0)
+	if mission.Participants == nil {
+		u := &User{}
+		mission.Participants = []*User{u}
+	}
 	mission.Creator = &User{}
 	if err = database.DB.Create(&mission).Error; err == nil {
 		mission.Creator.ID = mission.CreatorID
