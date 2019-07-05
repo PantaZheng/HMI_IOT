a5e4063c3e5b7dab3917492a415c05d0ca62aa38
PantaZheng <pantazheng@vip.qq.com>
2019-07-05 19:39:19 +0800

    测试json接口

--- models/gain.go
+++ models/gain.go
@@ -43,6 +43,7 @@ func gainTestData() {
 	gains := make([]Gain, l)
 	for i := 0; i < l; i++ {
 		gains[i].Name = "gain" + strconv.Itoa(i)
+		gains[i].MissionID = uint((i/2 + 1))
 	}
 
 	for _, v := range gains {
