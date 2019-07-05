fc953a443beb8b27d69780f37342e429fa1d3b1a
PantaZheng <pantazheng@vip.qq.com>
2019-07-05 19:55:59 +0800

    测试json接口

--- models/gain.go
+++ models/gain.go
@@ -8,33 +8,33 @@ import (
 )
 
 type GainCore struct {
-	ID        uint   `gorm:"primary_key";json:"id"`
+	ID        uint   `json:"id",gorm:"primary_key"`
 	Name      string `json:"name"`
 	State     uint   `json:"state"`
-	OwnerName string `gorm:"-";json:"ownerName"`
+	OwnerName string `json:"ownerName",gorm:"-"`
 }
 
 type Gain struct {
 	GainCore
 	CreatedAt  time.Time  `json:"-"`
-	CreateTime string     `gorm:"-";json:"createTime"`
+	CreateTime string     `json:"createTime",gorm:"-"`
 	UpdatedAt  time.Time  `json:"-"`
-	UpdateTime string     `gorm:"-";json:"updateTime"`
-	DeletedAt  *time.Time `sql:"index";json:"-"`
+	UpdateTime string     `json:"updateTime",gorm:"-"`
+	DeletedAt  *time.Time `sql:"index",json:"-"`
 	File       string     `json:"file"`
 	Remark     string     `json:"remark"`
 
 	MissionID   uint   `json:"missionID"`
-	MissionName string `gorm:"-";json:"missionName"`
+	MissionName string `json:"missionName",gorm:"-"`
 	OwnerID     uint   `json:"ownerID"`
 	ModuleID    uint   `json:"moduleID"`
-	ModuleName  string `gorm:"-";json:"moduleName"`
+	ModuleName  string `json:"moduleName",gorm:"-"`
 	LeaderID    uint   `json:"leaderID"`
-	LeaderName  string `gorm:"-";json:"leaderName"`
+	LeaderName  string `json:"leaderName,"gorm:"-"`
 	ProjectID   uint   `json:"projectID"`
-	ProjectName string `gorm:"-";json:"projectName"`
+	ProjectName string `json:"projectName",gorm:"-"`
 	ManagerID   uint   `json:"managerID"`
-	ManagerName string `gorm:"-";json:"managerName"`
+	ManagerName string `json:"managerName",gorm:"-"`
 }
 
 func gainTestData() {
